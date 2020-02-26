1b456f04d63ac7ea966c84fe4fd7bd3b2a49f9f8
bit01class <bit01class@gmail.com>
2020-02-26 10:46:10 +0900

    java config

---
+++ sts14/src/main/java/com/bit/last/config/ServletConfig.java
@@ -0,0 +1,49 @@
+package com.bit.last.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.bit.last"})
+public class ServletConfig implements WebMvcConfigurer {
+	// servlet-context.xml
+	/*
+	<resources mapping="/resources/**" location="/resources/" />
+	*/
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver resolver=null;
+		resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		registry.viewResolver(resolver);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sts14/src/main/java/com/bit/last/config/WebConfig.java
@@ -0,0 +1,65 @@
+package com.bit.last.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+// 3.1
+//interface WebApplicationInitializer
+// 3.2~
+public class WebConfig 
+extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		/*
+		<!-- The definition of the Root Spring Container shared by all Servlets and Filters -->
+		<context-param>
+			<param-name>contextConfigLocation</param-name>
+			<param-value>/WEB-INF/spring/root-context.xml</param-value>
+		</context-param>
+		
+		<!-- Creates the Spring Container shared by all Servlets and Filters -->
+		<listener>
+			<listener-class>org.springframework.web.context.ContextLoaderListener</listener-class>
+		</listener>
+		*/
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		/*
+		<servlet>
+		<servlet-name>appServlet</servlet-name>
+		<servlet-class>org.springframework.web.servlet.DispatcherServlet</servlet-class>
+		<init-param>
+			<param-name>contextConfigLocation</param-name>
+			<param-value>/WEB-INF/spring/appServlet/servlet-context.xml</param-value>
+		</init-param>
+		<load-on-startup>1</load-on-startup>
+	</servlet>
+		*/
+		System.out.println("getServletConfigClasses run...");
+		return new Class[]{ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// <servlet-mapping>
+		//		<servlet-name>appServlet</servlet-name>
+		//		<url-pattern>/</url-pattern>
+		//	</servlet-mapping>
+
+		return new String[] {"/"};
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
